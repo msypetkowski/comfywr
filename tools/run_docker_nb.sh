bee6e1151b5dc94ca33a50aeacb318168db21cbe
Michał Sypetkowski <gjxsymi@protonmail.com>
2024-12-15 13:25:41 +0100

    Fix run docker nb

--- tools/run_docker_nb.sh
+++ tools/run_docker_nb.sh
@@ -1,13 +1,9 @@
-dest_data_root=/workdir/ComfyUI/models/
+DEST_MODELS_ROOT=/workdir/ComfyUI/models/
 docker run --gpus all --rm --ipc=host --net=host \
     -v `pwd`:/workdir/ \
-    -v $(pwd)/custom_nodes/:/workdir/ComfyUI/custom_nodes/ \
-    -v $MY_DATA_ROOT/models/Stable-diffusion/:$dest_data_root/checkpoints/ \
-    -v $MY_DATA_ROOT/models/Lora/:/$dest_data_root/loras/ \
-    -v $MY_DATA_ROOT/VAE/:/$dest_data_root/vae/ \
-    -v $MY_DATA_ROOT/embeddings/:/$dest_data_root/embeddings/ \
-    -v $MY_DATA_ROOT/RealESRGAN/:/$dest_data_root/upscale_models/ \
-    -v $MY_DATA_ROOT/ControlNet/:/$dest_data_root/controlnet/ \
+    -v $(pwd)/custom_nodes/ComfyUI_UltimateSDUpscale/:/workdir/ComfyUI/custom_nodes/ComfyUI_UltimateSDUpscale/ \
+    -v $(pwd)/custom_nodes/ComfyUI-DepthAnythingV2/:/workdir/ComfyUI/custom_nodes/ComfyUI-DepthAnythingV2/ \
+    -v $(pwd)/downloaded_models/:$DEST_MODELS_ROOT \
     -v $(pwd)/comfywr_cache/:/root/ \
     -it comfywr:latest \
     bash -c "cd /workdir/; jupyter-lab --ip 0.0.0.0 --port 12345 --allow-root"
