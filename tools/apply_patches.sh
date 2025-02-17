26ff17ccd201e7ea777db52395294ef03a1a38bc
Michał Sypetkowski <gjxsymi@protonmail.com>
2025-02-17 17:48:29 +0100

    Update apply patches

--- tools/apply_patches.sh
+++ tools/apply_patches.sh
@@ -7,6 +7,11 @@ git stash
 git apply ../../patches/comfyui_3d_pack.diff
 popd
 
+pushd custom_nodes/comfyui_marigold/
+git stash
+git apply ../../patches/marigold.diff
+popd
+
 pushd custom_nodes/ComfyUI-ModelUnloader//
 git stash
 git apply ../../patches/model_unloader.diff
