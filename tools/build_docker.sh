b2e239401c5fac245d684ad805ebf95f58a50554
Michał Sypetkowski <gjxsymi@protonmail.com>
2025-04-01 10:03:23 +0200

    Try make Hi3DGen work

--- tools/build_docker.sh
+++ tools/build_docker.sh
@@ -6,11 +6,11 @@ TARGET_IMAGE="comfywr:latest"
 
 docker build -t $BASE_IMAGE -f docker/comfywr/Dockerfile .
 
-CONTAINER_NAME=tmp_comfywr_name
-docker run --gpus all --name $CONTAINER_NAME -it $BASE_IMAGE /bin/bash -c "cd /install_dir/ && python install.py && python -m pip install kiui"
-sleep 2
-docker commit $CONTAINER_NAME $TARGET_IMAGE
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
-
-echo "Build completed. New image created: $TARGET_IMAGE"
+# CONTAINER_NAME=tmp_comfywr_name
+# docker run --gpus all --name $CONTAINER_NAME -it $BASE_IMAGE /bin/bash -c "cd /install_dir/ && python install.py && python -m pip install kiui"
+# sleep 2
+# docker commit $CONTAINER_NAME $TARGET_IMAGE
+# docker stop $CONTAINER_NAME
+# docker rm $CONTAINER_NAME
+# 
+# echo "Build completed. New image created: $TARGET_IMAGE"
